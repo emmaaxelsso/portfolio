953d5b2f8662a426f7fc0722e17edfabf910804b
emmaaxelsso <93664659+emmaaxelsso@users.noreply.github.com>
2026-04-04 16:47:01 +0200

    feat: add high-contrast color system and hero styling

--- my-portfolio/src/app/features/home/ components/hero/hero.component.ts
+++ my-portfolio/src/app/features/home/ components/hero/hero.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.scss'
 })
 export class HeroComponent {}

6af2d7fc9bc6ea07608cfc396d9a43e8ea7ccceb
emmaaxelsso <93664659+emmaaxelsso@users.noreply.github.com>
2026-04-06 15:08:02 +0200

    fetching data from file instead of being hard coded into the cards

--- my-portfolio/src/app/core/constants/projects.data.ts
+++ my-portfolio/src/app/core/constants/projects.data.ts
@@ -1,10 +1,17 @@
 export const PROJECTS = [
   {
     title: 'Portfolio',
-    description: 'Min personliga portfoliosida byggd i Angular.'
+    slug: 'portfolio',
+    description: 'This webpage! A portfolio for myself.'
   },
   {
-    title: 'Todo-app',
-    description: 'En enkel app för att hantera uppgifter.'
+    title: 'An Interactive Language Map',
+    slug: 'interactive-language-map',
+    description: 'A project for my bachelors thesis. A language map on region level that fetched data from wikidata and displayed it on an interactive map.'
+  },
+  {
+    title: 'Eventi',
+    slug: 'eventi',
+    description: 'A project where we worked according to the agile principles to create a website for events.'
   }
 ];
--- my-portfolio/src/app/features/home/ components/projects-preview/projects-preview.component.ts
+++ my-portfolio/src/app/features/home/ components/projects-preview/projects-preview.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SectionTitleComponent } from '../../../../shared/components/section-title/section-title.component';
-import { ProjectCardComponent } from '../../../../shared/components/project-card/project-card.component';
 import { PROJECTS } from '../../../../core/constants/projects.data';
 
 @Component({
